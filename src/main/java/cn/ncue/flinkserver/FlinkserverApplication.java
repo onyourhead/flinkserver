5ea9dfd6f57326c8ae8f75adc1b72c6bb019e11d
张政淇 <1548069580@qq.com>
2019-10-13 17:01:19 +0800

    Initial commit

---
+++ src/main/java/cn/ncue/flinkserver/FlinkserverApplication.java
@@ -0,0 +1,13 @@
+package cn.ncue.flinkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkserverApplication.class, args);
+    }
+
+}
